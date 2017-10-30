e532bc0ad9d93fa9a40fdf4627452f8b5e006340
Miro Alt <you@example.com>
2017-10-30 22:12:20 +0100

    Project update

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,48 @@
-svg4everybody();
+(function ($) {
+
+    //svg4everybody();
+
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 5,
+        spaceBetween: 10,
+        slidesPerGroup: 3,
+        loop: true,
+        loopFillGroupWithBlank: false,
+        //dynamicBullets: true,
+        grabCursor: true,
+        /*navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        },*/
+        /*pagination: {
+            el: '.swiper-pagination',
+            //type: 'progressbar',
+            clickable: true
+        },*/
+        /*autoplay: {
+            delay: 1500,
+            disableOnInteraction: false
+        }*/
+    });
+
+
+
+
+})(jQuery);
+
+/*Fancy box*/
+
+$(document).ready(function() {
+
+    /*menu*/
+    var $menu = $('#menu'),
+        $menulink = $('.menu-link');
+
+    $menulink.click(function() {
+        $menulink.toggleClass('active');
+        $menu.toggleClass('active');
+        return false;
+    });
+
+    $(".fancybox").fancybox();
+});
